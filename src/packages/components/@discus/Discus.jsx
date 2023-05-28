0225b25159ff92819162d257aeee639dd5a338eb
dsva <darwin97.va@gmail.com>
2023-05-28 17:26:11 -0500

    discus

---
+++ src/packages/components/@discus/Discus.jsx
@@ -0,0 +1,27 @@
+import { DiscussionEmbed } from "disqus-react";
+import { useEffect, useState } from "react";
+
+const Discus = () => {
+  const [href, setHref] = useState("");
+
+  useEffect(() => {
+    setHref(window.location.href);
+  }, []);
+
+  return (
+    <div className="mt-4 mx-[3rem]">
+      {href ? (
+        <DiscussionEmbed
+          shortname="darwinva"
+          config={{
+            url: href,
+            identifier: "1",
+            title: "Aimet Clients",
+          }}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Discus;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import MainLayout from "@/layouts/MainLayout";
 import About from "@/packages/components/@about/About";
 import Contact from "@/packages/components/@contact/Contact";
 import FbComments from "@/packages/components/@fbcomments/FbComments";
+import Discus from "@/packages/components/@discus/Discus";
 import Hero from "@/packages/components/@hero/Hero";
 import Paquetes from "@/packages/components/@paquetes/Paquetes";
 import Portfolio from "@/packages/components/@portfolio/Portfolio";
@@ -16,11 +17,11 @@ export default function Home() {
           <About />
           <Paquetes />
           <Portfolio />
+          <Contact />
           <h2 className="text-3xl text-center mt-[4rem] mb-5">
             Lo que dicen nuestros clientes
           </h2>
-          <FbComments className="m-4" />
-          <Contact />
+          <Discus />
         </div>
       </MainLayout>
       <Toaster />
